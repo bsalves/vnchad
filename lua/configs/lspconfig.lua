f0b95156a68f87b6e6f024058c3f0b93fa2a049f
Bruno Alves <bsalvesdev@gmail.com>
2025-05-27 11:11:42 -0300

    fix: lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "sourcekit" }
+local servers = { "html", "cssls", "sourcekit", "ts_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
